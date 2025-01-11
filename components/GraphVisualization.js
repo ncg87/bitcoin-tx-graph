d4be01a18210e557cd0b77c4249946861fef1d00
ncg87 <115420183+ncg87@users.noreply.github.com>
2025-01-10 20:59:09 -0500

    little update

--- components/GraphVisualization.js
+++ components/GraphVisualization.js
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Switch } from '@/components/ui/switch';
