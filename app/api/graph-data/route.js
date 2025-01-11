9d160e7e49bd4479698878132d507c665ae37cdc
ncg87 <115420183+ncg87@users.noreply.github.com>
2025-01-11 00:45:07 -0500

    uses .env now

--- app/api/graph-data/route.js
+++ app/api/graph-data/route.js
@@ -1,9 +1,12 @@
 import { NextResponse } from 'next/server';
 import neo4j from 'neo4j-driver';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const driver = neo4j.driver(
-  'bolt://localhost:7687',
-  neo4j.auth.basic('neo4j', 'password'),
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD),
   {
     encrypted: false,
     trust: 'TRUST_ALL_CERTIFICATES',
