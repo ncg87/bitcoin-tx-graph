0be30e3a6e13ee1aef04f9760b9484d60ef44ca3
ncg87 <115420183+ncg87@users.noreply.github.com>
2025-01-11 00:50:23 -0500

    little changes

--- app/api/graph-data/route.js
+++ app/api/graph-data/route.js
@@ -3,10 +3,9 @@ import neo4j from 'neo4j-driver';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
-  neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD),
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
   {
     encrypted: false,
     trust: 'TRUST_ALL_CERTIFICATES',
